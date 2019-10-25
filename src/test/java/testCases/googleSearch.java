686a90fff8d92ac50bfbaea73dace080d1758d74
Pravin Sonawane <sonapravin@gmail.com>
2019-10-25 11:23:08 +0530

    initial commit

--- src/test/java/testCases/googleSearch.java
+++ src/test/java/testCases/googleSearch.java
@@ -2,21 +2,27 @@ package testCases;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class googleSearch {
+
+	final static 	Logger logger = Logger.getLogger(googleSearch.class);
 	
 	@Test
 	public void search() {
-
 		
-		  File chromeDriver = new File("/home/pravin/git/SELENIUM_AUTOMATION/seleniumProject1/resources/chromedriver_linux64/chromedriver");
-		   System.setProperty("webdriver.chrome.driver", chromeDriver.getAbsolutePath());
-		  WebDriver driver = new ChromeDriver();
-		  driver = new ChromeDriver();
-			driver.get("https://www.google.com/");
+	
+		File chromeDriver = new File(
+				"/home/pravin/git/SELENIUM_AUTOMATION/seleniumProject1/resources/chromedriver");
+		System.setProperty("webdriver.chrome.driver", chromeDriver.getAbsolutePath());
+		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
+		
+		logger.info("Opening google.com");
+		driver.get("https://www.google.com/");
 	}
 
 }
